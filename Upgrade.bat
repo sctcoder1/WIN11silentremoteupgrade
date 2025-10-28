0b79110ad7e0366dc7e9fa986af3ac5b76063f71
sctcoder1 <bdooley@courts.ms.gov>
2025-10-28 10:08:03 -0500

    Update Upgrade.bat

--- Upgrade.bat
+++ Upgrade.bat
@@ -1,16 +1,10 @@
 @echo off
 title Windows 11 In-Place Upgrade
 color 0A
-
 set "root=C:\Win11Upgrade"
 set "ps1=%root%\Upgrade.ps1"
-set "log=%root%\upgrade_runner.log"
 
-echo [%date% %time%] Starting Upgrade Runner >> "%log%"
-echo Running PowerShell script... >> "%log%"
-
-:: Heartbeat (so Task Scheduler stays alive)
-powershell -ExecutionPolicy Bypass -NoProfile -File "%ps1%" >> "%log%" 2>&1
-
-echo [%date% %time%] Script completed. >> "%log%"
+echo [%date% %time%] Starting Upgrade.ps1 >> "%root%\upgrade_runner.log"
+powershell -ExecutionPolicy Bypass -NoProfile -File "%ps1%" >> "%root%\upgrade_runner.log" 2>&1
+echo [%date% %time%] Upgrade.ps1 finished. >> "%root%\upgrade_runner.log"
 exit /b 0
